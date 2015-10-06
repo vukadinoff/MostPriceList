ba6e2a15a8486c80c78368df4bbbabe846f6ead4
vukadinoff <stiliyan.vukadinov@maniaprint.bg>
2015-10-06 12:58:32 +0300

    add main unit changes

--- FrameMostPriceList.pas
+++ FrameMostPriceList.pas
@@ -11,11 +11,44 @@ type
   private
     { Private declarations }
   public
-    { Public declarations }
-  end;
+    procedure Notifier_RefreshAll;
+    procedure Notifier_PrintReport;
+		procedure Notifier_ExportReport(const aExportFmt:Integer);
+end;
 
 implementation
 
 {$R *.dfm}
+
+procedure TFrameMostPriceListF.Notifier_PrintReport;
+// var lvLeftTitle:string;
+begin
+  inherited;
+  //PrinterG1.ReportTitle.Text:=Format('Справка "%s"', [Self.Caption]);
+  {if(FPeriodFrame1.Active)then
+      lvLeftTitle:=Format('Период от %s до %s',  [FPeriodFrame1.StartPeriod_AsText, FPeriodFrame1.EndPeriod_AsText]);
+  Printer1G1.PrinterPage.PageHeader.LeftTitle.Text:=lvLeftTitle;
+  Printer1G1.PrinterPage.PageHeader.RightTitle.Text:='В обекти: ' + FCoffeePanel1.CheckedItemsAsText;}
+//  PrinterG1.Preview();
+end;
+
+procedure TFrameMostPriceListF.Notifier_RefreshAll;
+var lviOrderId: Integer;
+begin
+//  ExecuteQueryOrders;
+
+//  lviOrderId := GetSelectedOrderId;
+//  if (lviOrderId > -1) then
+//    ExecuteQueryItems(lviOrderId)
+//  else
+//    G2V1.ClearItems;
+
+end;
+
+procedure TFrameMostPriceListF.Notifier_ExportReport(const aExportFmt: Integer);
+begin
+  inherited;
+//  CommonExports.ExportGridTo(G1);
+end;
 
 end.
--- MainFUnit.pas
+++ MainFUnit.pas
@@ -3,22 +3,86 @@ unit MainFUnit;
 interface
 
 uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  ActnList, Classes, SysUtils, DateUtils, Controls, ExtCtrls, Forms, ImgList,
+  dxSkinsdxBarPainter, FrameMostPriceList, dxSkinsCore,
+  dxSkinsDefaultPainters, dxBar, cxClasses;
 
 type
   TMainF = class(TForm)
+    AL1          : TActionList;
+    actClose     : TAction;
+    actRefresh   : TAction;
+    actPrint     : TAction;
+    actExport    : TAction;
+
+    BM1          : TdxBarManager;
+    BM1Bar1      : TdxBar;
+    btnClose     : TdxBarLargeButton;
+    btnRefresh   : TdxBarLargeButton;
+    btnPrint     : TdxBarLargeButton;
+    btnExport    : TdxBarLargeButton;
+    ilImages     : TImageList;
+
+    pnlG1G2      : TPanel;
+
+    procedure FormCreate(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+
+    procedure actCloseExecute(Sender: TObject);
+    procedure actRefreshExecute(Sender: TObject);
+    procedure actPrintExecute(Sender: TObject);
+    procedure actExportExecute(Sender: TObject);
   private
-    { Private declarations }
-  public
-    { Public declarations }
-  end;
+    FrameMostPriceList: TFrameMostPriceListF; //Frame instance variable end;
+end;
 
 var
   MainF: TMainF;
 
 implementation
 
+uses
+  MLDMS_CommonConstants;
+
 {$R *.dfm}
+
+procedure TMainF.FormCreate(Sender: TObject);
+begin
+  FrameMostPriceList := TFrameMostPriceListF.Create(MainF);
+  FrameMostPriceList.Parent := pnlG1G2;
+end;
+
+procedure TMainF.FormActivate(Sender: TObject);
+begin
+  //FrameMostPriceList.G1.SetFocus;
+  //FrameMostPriceList.G1V1.DataController.FocusedRowIndex := 0;
+end;
+
+procedure TMainF.FormDestroy;
+begin
+  if (Assigned(FrameMostPriceList)) then
+    FrameMostPriceList.Free;
+end;
+
+procedure TMainF.actCloseExecute(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TMainF.actRefreshExecute(Sender: TObject);
+begin
+  FrameMostPriceList.Notifier_RefreshAll;
+end;
+
+procedure TMainF.actPrintExecute(Sender: TObject);
+begin
+  FrameMostPriceList.Notifier_PrintReport;
+end;
+
+procedure TMainF.actExportExecute(Sender: TObject);
+begin
+  FrameMostPriceList.Notifier_ExportReport(cUnknownID);
+end;
 
 end.
