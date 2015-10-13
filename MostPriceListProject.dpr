a9dd9473333d6ff9ac92ce9eb6384c447e57447d
vukadinoff <stiliyan.vukadinov@maniaprint.bg>
2015-10-13 09:41:59 +0300

    unmerge

--- MostPriceListProject.dpr
+++ MostPriceListProject.dpr
@@ -5,13 +5,13 @@ uses
   MainFUnit in 'MainFUnit.pas' {MainF},
   FrameMostCategoryUnit in 'FrameMostCategoryUnit.pas' {FrameMostCategory: TFrame},
   ExchangeRatesFUnit in 'ExchangeRatesFUnit.pas' {ExchangeRatesF},
-  DataModule in 'DataModule.pas' {DM: TDataModule};
+  DataModule in 'Units\DataModule.pas' {DM: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TDM, DM);
   Application.CreateForm(TMainF, MainF);
+  Application.CreateForm(TDM, DM);
   Application.Run;
 end.
